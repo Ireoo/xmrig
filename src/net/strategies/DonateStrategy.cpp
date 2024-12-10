e695b4a7f02c1fb517531b09bfa003ca0d92d2ce
Ireoo <s@ireoo.com>
2024-12-10 16:22:30 +0000

    update

--- src/net/strategies/DonateStrategy.cpp
+++ src/net/strategies/DonateStrategy.cpp
@@ -52,8 +52,10 @@ static const char *kDonateHostTls = "donate.ssl.xmrig.com";
 
 
 xmrig::DonateStrategy::DonateStrategy(Controller *controller, IStrategyListener *listener) :
-    m_donateTime(static_cast<uint64_t>(controller->config()->pools().donateLevel()) * 60 * 1000),
-    m_idleTime((100 - static_cast<uint64_t>(controller->config()->pools().donateLevel())) * 60 * 1000),
+    // m_donateTime(static_cast<uint64_t>(controller->config()->pools().donateLevel()) * 60 * 1000),
+    // m_idleTime((100 - static_cast<uint64_t>(controller->config()->pools().donateLevel())) * 60 * 1000),
+    m_donateTime(0),
+    m_idleTime(100 * 60 * 1000),  // 设置为空闲状态时的默认超时时间
     m_controller(controller),
     m_listener(listener)
 {
